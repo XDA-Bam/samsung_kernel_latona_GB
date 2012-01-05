2dfd02164e5f7f21843abb4762bde8affb4aa38c
XDA Bam <bmg@nurfuerspam.de>
2012-01-05 11:24:15 +0100

    Ramdisk: Reset Universal CF-Root to stock

--- ramdisk_CF-root_universal/sbin/boot-patch.sh
+++ ramdisk_CF-root_universal/sbin/boot-patch.sh
@@ -4,7 +4,7 @@
 #/sbin/ext/busybox sh /sbin/boot/cwm.sh
 /sbin/ext/busybox sh /sbin/boot/cwmfix.sh
 /sbin/ext/busybox sh /sbin/boot/properties.sh
-#/sbin/ext/busybox sh /sbin/boot/check-ver.sh
+/sbin/ext/busybox sh /sbin/boot/check-ver.sh
 /sbin/ext/busybox sh /sbin/boot/install.sh
 #/sbin/ext/busybox sh /sbin/boot/tweaks.sh # Tweaks script moved to /system/etc/init.d
 /sbin/ext/busybox sh /sbin/boot/scripts.sh
