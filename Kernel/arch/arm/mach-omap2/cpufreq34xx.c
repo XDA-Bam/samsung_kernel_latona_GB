d4f82d52d5e23e8de0ad7bf3b23edb207cc14032
XDA Bam <bmg@nurfuerspam.de>
2012-03-08 09:33:02 +0100

    Overclock: Slight increase in OPP5 voltage

--- Kernel/arch/arm/mach-omap2/cpufreq34xx.c
+++ Kernel/arch/arm/mach-omap2/cpufreq34xx.c
@@ -91,9 +91,9 @@ static struct omap_opp_def __initdata omap36xx_opp_def_list[] = {
 	OMAP_OPP_DEF("mpu", true,  1000000000, 1350000),
 	/* MPU OPP5 */
 	#ifdef CONFIG_SAMSUNG_LATONA_OPP5_ENABLED
-	OMAP_OPP_DEF("mpu", true,  1200000000, 1380000),
+	OMAP_OPP_DEF("mpu", true,  1200000000, 1400000),
 	#else
-	OMAP_OPP_DEF("mpu", false,  1200000000, 1380000),
+	OMAP_OPP_DEF("mpu", false,  1200000000, 1400000),
 	#endif
 
 	/* L3 OPP1 - OPP50 */
@@ -111,9 +111,9 @@ static struct omap_opp_def __initdata omap36xx_opp_def_list[] = {
 	OMAP_OPP_DEF("iva", true, 800000000, 1350000),
 	/* DSP OPP5 */
 	#ifdef CONFIG_SAMSUNG_LATONA_OPP5_ENABLED
-	OMAP_OPP_DEF("iva", true, 840000000, 1380000),
+	OMAP_OPP_DEF("iva", true, 840000000, 1400000),
 	#else
-	OMAP_OPP_DEF("iva", false, 840000000, 1380000),
+	OMAP_OPP_DEF("iva", false, 840000000, 1400000),
 	#endif
 };
 static u32 omap36xx_opp_def_size = ARRAY_SIZE(omap36xx_opp_def_list);
