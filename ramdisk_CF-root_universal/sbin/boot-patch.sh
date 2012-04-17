c0193153cfaa4c2beb67c10f00815f9621a24b3f
XDA Bam <bmg@nurfuerspam.de>
2012-04-17 11:30:27 +0200

    Ramdisk: General housekeeping for CF-root

--- ramdisk_CF-root_universal/sbin/boot-patch.sh
+++ ramdisk_CF-root_universal/sbin/boot-patch.sh
@@ -1,16 +1,14 @@
 #!/sbin/ext/busybox sh
 
 /system/bin/logwrapper /sbin/ext/busybox sh /sbin/boot/busybox.sh
-/system/bin/logwrapper /sbin/ext/busybox sh /sbin/boot/cwmfix.sh
+/system/bin/logwrapper /sbin/ext/busybox sh /sbin/boot/swap-SD.sh
 /system/bin/logwrapper /sbin/ext/busybox sh /sbin/boot/properties.sh
 /system/bin/logwrapper /sbin/ext/busybox sh /sbin/boot/check-ver.sh
 /system/bin/logwrapper /sbin/ext/busybox sh /sbin/boot/install.sh
 # Tweaks script moved to /system/etc/init.d
-#/system/bin/logwrapper /sbin/ext/busybox sh /sbin/boot/tweaks.sh
+/system/bin/logwrapper /sbin/ext/busybox sh /sbin/boot/fix_permissions-patch.sh
 /system/bin/logwrapper /sbin/ext/busybox sh /sbin/boot/scripts.sh
 
-#fix_permissions patch
-/system/bin/logwrapper /sbin/ext/busybox sh /sbin/boot/fix_permissions-patch.sh
 
 read sync < /data/sync_fifo
 rm /data/sync_fifo
