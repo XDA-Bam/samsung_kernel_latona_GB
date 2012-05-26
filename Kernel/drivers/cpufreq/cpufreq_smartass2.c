f2f7dcd6708023627cc4a2ae6d2808d8ff7d449a
XDA Bam <bmg@nurfuerspam.de>
2012-05-26 17:54:45 +0200

    Changed smartassV2 settings after longterm  testing

--- Kernel/drivers/cpufreq/cpufreq_smartass2.c
+++ Kernel/drivers/cpufreq/cpufreq_smartass2.c
@@ -52,7 +52,7 @@ static unsigned int awake_ideal_freq;
  * that practically when sleep_ideal_freq==0 the awake_ideal_freq is used
  * also when suspended).
  */
-#define DEFAULT_SLEEP_IDEAL_FREQ 300000
+#define DEFAULT_SLEEP_IDEAL_FREQ 0
 static unsigned int sleep_ideal_freq;
 
 /*
@@ -101,7 +101,7 @@ static unsigned long down_rate_us;
  * The frequency to set when waking up from sleep.
  * When sleep_ideal_freq=0 this will have no effect.
  */
-#define DEFAULT_SLEEP_WAKEUP_FREQ 1000000
+#define DEFAULT_SLEEP_WAKEUP_FREQ 600000
 static unsigned int sleep_wakeup_freq;
 
 /*
