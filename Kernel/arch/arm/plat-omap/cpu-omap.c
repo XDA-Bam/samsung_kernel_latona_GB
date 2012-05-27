ce963931ba19068dc85613c3356a623c060be945
XDA Bam <bmg@nurfuerspam.de>
2012-05-27 20:41:36 +0200

    Limit startup frequency to 1 GHz as soon as cpu is initialized

--- Kernel/arch/arm/plat-omap/cpu-omap.c
+++ Kernel/arch/arm/plat-omap/cpu-omap.c
@@ -245,7 +245,11 @@ static int omap_cpu_init(struct cpufreq_policy *policy)
 	}
 
 	policy->min = policy->cpuinfo.min_freq;
+#ifdef CONFIG_SAMSUNG_LATONA_OPP5_ENABLED
+	policy->max = 1000000;
+#else
 	policy->max = policy->cpuinfo.max_freq;
+#endif
 	policy->cur = omap_getspeed(policy->cpu);
 
 	/* Program the actual transition time for worstcase */
