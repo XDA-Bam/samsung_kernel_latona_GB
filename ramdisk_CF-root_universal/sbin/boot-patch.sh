0b00c5a9f8f8c058c2bb87eeab18bc2227af4f0e
XDA Bam <bmg@nurfuerspam.de>
2012-06-23 16:36:00 +0200

    Ramdisk: Add XXKPQ WiFi modules and change WiFi patching in universal CF-root

--- ramdisk_CF-root_universal/sbin/boot-patch.sh
+++ ramdisk_CF-root_universal/sbin/boot-patch.sh
@@ -3,7 +3,7 @@
 /system/bin/logwrapper /sbin/ext/busybox sh /sbin/boot/busybox.sh
 /system/bin/logwrapper /sbin/ext/busybox sh /sbin/boot/swap-SD.sh
 /system/bin/logwrapper /sbin/ext/busybox sh /sbin/boot/properties.sh
-/system/bin/logwrapper /sbin/ext/busybox sh /sbin/boot/check-ver.sh
+/system/bin/logwrapper /sbin/ext/busybox sh /sbin/boot/fix_wifi.sh
 /system/bin/logwrapper /sbin/ext/busybox sh /sbin/boot/install.sh
 # Tweaks script moved to /system/etc/init.d
 /system/bin/logwrapper /sbin/ext/busybox sh /sbin/boot/fix_permissions-patch.sh
---
+++ ramdisk_CF-root_universal/sbin/boot/fix_wifi.sh
@@ -0,0 +1,32 @@
+TOOLBOX=/sbin/ext/busybox
+
+KFILE="/lib/modules/2.6.35.7/kernel/drivers/scsi/scsi_wait_scan.ko"
+SORIG=`strings $KFILE |grep vermagic=2.6`
+SORIG=`echo $SORIG | cut -d'-' -f2 | cut -d' ' -f1`
+SDIFF=`strings /system/etc/wifi/tiwlan_drv.ko |grep vermagic=2.6`
+SDIFF=`echo $SDIFF | cut -d'-' -f2 | cut -d' ' -f1`
+
+if [ $SDIFF != $SORIG ]; then
+# Remount data & system RW
+    $TOOLBOX mount -o remount,rw /system
+    $TOOLBOX mount -o remount,rw /data
+
+# Remove the cf-root installed flag
+    $TOOLBOX rm -fr /system/cfroot/
+
+# Backup original WiFi modules
+    $TOOLBOX cat /system/etc/wifi/tiwlan_drv.ko > /data/cfroot/backup/tiwlan_drv.ko
+    $TOOLBOX cat /system/etc/wifi/softap/tiap_drv.ko > /data/cfroot/backup/tiap_drv.ko
+
+# Replace WiFi modules
+    $TOOLBOX cat /res/misc/tiwlan_drv.ko > /system/etc/wifi/tiwlan_drv.ko
+    $TOOLBOX cat /res/misc/tiap_drv.ko > /system/etc/wifi/softap/tiap_drv.ko
+    $TOOLBOX chown 0.0 /system/etc/wifi/tiwlan_drv.ko
+    $TOOLBOX chown 0.0 /system/etc/wifi/softap/tiap_drv.ko
+    $TOOLBOX chmod 644 /system/etc/wifi/tiwlan_drv.ko
+    $TOOLBOX chmod 644 /system/etc/wifi/softap/tiap_drv.ko
+
+# Remount data & system RO
+    $TOOLBOX mount -o remount,ro /system
+    $TOOLBOX mount -o remount,ro /data
+fi;
