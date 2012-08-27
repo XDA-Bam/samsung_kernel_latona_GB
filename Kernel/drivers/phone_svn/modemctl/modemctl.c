4d1b2e4af9c8de9f627452b15b014fae869004b6
XDA Bam <bmg@nurfuerspam.de>
2012-08-27 22:20:40 +0200

    Revert "Sync bug fix: Move call of process_whilte_list() back to wakelock.c"
    This reverts commit 6d8d94144985767ec89a9055f5b9979f6cf0c7a5.

--- Kernel/drivers/phone_svn/modemctl/modemctl.c
+++ Kernel/drivers/phone_svn/modemctl/modemctl.c
@@ -47,14 +47,14 @@
 #endif
 
 #ifdef CONFIG_SVNET_WHITELIST
-#include <linux/phone_svn/portlist.h>
+#include "portlist.h"
 #endif /* CONFIG_SVNET_WHITELIST */
 
 #define DRVNAME "modemctl"
 
 //#define USE_EARLYSUSPEND_TO_CTRL_PDAACTIVE_LOW
-//#define USE_LATERESUME_TO_CTRL_PDAACTIVE_HIGH
-//#define USE_EARLYSUSPEND_TO_CTRL_WHITELIST
+#define USE_LATERESUME_TO_CTRL_PDAACTIVE_HIGH
+#define USE_EARLYSUSPEND_TO_CTRL_WHITELIST
 
 //#define SIM_DEBOUNCE_TIME_HZ	(HZ)
 
@@ -969,7 +969,6 @@ static int modemctl_suspend(struct platform_device *pdev, pm_message_t state)
 	pda_off(mc);
 #endif
 
-#if 0
 #if defined( CONFIG_SVNET_WHITELIST ) && \
 	!defined( USE_EARLYSUSPEND_TO_CTRL_WHITELIST )
 	// call process white list
@@ -977,7 +976,6 @@ static int modemctl_suspend(struct platform_device *pdev, pm_message_t state)
 	if( unlikely( retval !=0 ) ) {
 		printk( "fail to send whitelist : %d\n", retval );
 	} 
-#endif
 #endif
 
 	return 0;
